94860d3810e55b96acc68ec688732c5bbc4d43cb
Jin Kang <kbjss071@gmail.com>
2023-03-14 23:26:31 -0400

    fixed urls for simple social

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'groups',
-    'posts'
+    'posts',
     'accounts',
 ]
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
