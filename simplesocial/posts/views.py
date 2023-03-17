fd02bc6749f9eafb0daeef07db998296a9c60799
Jin Kang <kbjss071@gmail.com>
2023-03-17 19:35:35 -0400

    fixed issues in post model

--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
